e670c3bd6218899326fe26475d1cfce83a635389
Vanessa Nascimento <101233011+Nayayaa@users.noreply.github.com>
2025-05-15 11:53:17 -0300

    Exercício de polimorfismo
    Exercício para compreender como funciona o polimorfismo em Java.

---
+++ polimorfismo/src/sobreescrita/Animal.java
@@ -0,0 +1,7 @@
+package sobreescrita;
+
+public class Animal {
+    public void emitirSom() {
+        System.out.println("O animal faz um som.");
+    }
+}
---
+++ polimorfismo/src/teste/LuluDaPomerania.java
@@ -0,0 +1,11 @@
+package teste;
+
+import sobreescrita.Cachorro;
+
+public class LuluDaPomerania extends Cachorro {
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Lulu");
+    }
+}
